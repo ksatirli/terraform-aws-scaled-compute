c4211bcfeb4ae97838b6caaf70f11b213bb59562
Kerim Satirli <k.satirli@gmail.com>
2023-11-12 17:48:39 -0800

    work-up for example

---
+++ examples/basic/terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
+  required_providers {
+    # see https://registry.terraform.io/providers/hashicorp/aws/5.25.0/
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.25.0, < 6.0.0"
+    }
+  }
+
+  # see https://developer.hashicorp.com/terraform/language/settings#specifying-a-required-terraform-version
+  required_version = ">= 1.6.0"
+}
